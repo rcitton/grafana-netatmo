9a9e99e0daa8bf1a4d6959d0e4214f774ed350db
Ruggero Citton <ruggero.citton@oracle.com>
2023-05-19 18:15:38 +0200

    Add files via upload

--- netatmo_influx.py
+++ netatmo_influx.py
@@ -4,10 +4,19 @@
 import lnetatmo
 from influxdb import InfluxDBClient
 
+# Netatmo configuration - edit these
 CLIENT_ID = ''
 CLIENT_SECRET = ''
 USERNAME = ''
 PASSWORD = ''
+
+# influx configuration - edit these
+ifuser = ''
+ifpass = ''
+ifdb   = ''
+ifhost = '127.0.0.1'
+ifport = 8086
+
 
 authorization = lnetatmo.ClientAuth(
         clientId=CLIENT_ID,
@@ -16,12 +25,12 @@ authorization = lnetatmo.ClientAuth(
         password=PASSWORD,
         scope='read_station'
         )
-
 weatherData = lnetatmo.WeatherStationData(authorization)
 
-client = InfluxDBClient()
-if {'name': 'netatmo'} not in client.get_list_database():
-    client.create_database('netatmo')
+
+client = InfluxDBClient(ifhost,ifport,ifuser,ifpass,ifdb)
+if {'name': ifdb} not in client.get_list_database():
+    client.create_database(ifdb)
 
 for station in weatherData.stations:
     station_data = []
@@ -33,7 +42,8 @@ for station in weatherData.stations:
     timezone = station['place']['timezone']
     longitude = station['place']['location'][0]
     latitude = station['place']['location'][1]
-    for module, moduleData in weatherData.lastData(station=station_name, exclude=3600).items():
+    weatherData.default_station_data = weatherData.stationByName(station_name)
+    for module, moduleData in weatherData.lastData(exclude=3600).items():
         for measurement in ['altitude', 'country', 'longitude', 'latitude', 'timezone']:
             value = eval(measurement)
             if type(value) == int:
@@ -66,5 +76,5 @@ for station in weatherData.stations:
                     }
                 })
 
-    client.write_points(station_data, time_precision='s', database='netatmo')
-    client.write_points(module_data, time_precision='s', database='netatmo')
+    client.write_points(station_data, time_precision='s', database=ifdb)
+    client.write_points(module_data, time_precision='s', database=ifdb)
